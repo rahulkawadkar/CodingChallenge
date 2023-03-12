49daeba83d859888930e10f90ac3e47bb481023b
Rahul Kawadkar <rahul.2605@yahoo.co.in>
2023-03-12 09:50:13 +0530

    commit changes to backend configs.

--- Challenge1/backend.tf
+++ Challenge1/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "ajit-inamdar-tech-terraform-backend"
+    bucket         = "terraform-backend"
     encrypt        = true
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "ajit-inamdar-tech-terraform-backend"
+    dynamodb_table = "terraform-backend"
   }
 }
